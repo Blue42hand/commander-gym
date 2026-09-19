f2cdb9878ce54940cb02089b9e8c535b8745b9d2
Blue42hand <flamingheads@gmail.com>
2026-09-19 11:30:00 -0700

    Export structured decision records

--- commander_gym/__init__.py
+++ commander_gym/__init__.py
@@ -53,13 +53,20 @@ from .pilot_records import (
     PilotRecordContext,
     PilotRecordError,
     decision_record_from_execution_trace,
+    structured_decision_record_from_execution_trace,
 )
 from .pilot_routing import (
     CertifiedMechanicalHandler,
     ForcedParameterlessChoiceHandler,
     RoutingPilot,
 )
-from .records import ActionRecord, DecisionRecord, PilotProvenance, RecordValidationError
+from .records import (
+    ActionRecord,
+    DecisionRecord,
+    PilotProvenance,
+    RecordValidationError,
+    StructuredDecisionRecord,
+)
 from .run_records import (
     RUN_RECORD_SCHEMA_VERSION,
     RUN_STATUS_COMPLETED,
@@ -129,9 +136,11 @@ __all__ = [
     "RunTermination",
     "ServerIdentity",
     "SparseDecisionFeatures",
+    "StructuredDecisionRecord",
     "StaleArgentumDecisionError",
     "choose_for_observation",
     "decision_record_from_execution_trace",
+    "structured_decision_record_from_execution_trace",
     "encode_decision_record",
     "first_legal_pilot",
     "policy_family_for_decision_type",
