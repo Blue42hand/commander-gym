66c8fcad038cbdff6f67d15ceab3bfe27695e39a
Blue42hand <flamingheads@gmail.com>
2026-09-18 17:13:16 -0700

    Export Argentum HTTP client interfaces

--- commander_gym/__init__.py
+++ commander_gym/__init__.py
@@ -5,12 +5,26 @@ from .argentum import (
     ArgentumCanonicalError,
     StaleArgentumDecisionError,
 )
+from .argentum_client import (
+    ArgentumClientConfigurationError,
+    ArgentumClientError,
+    ArgentumConnectionError,
+    ArgentumDeliveryUnknownError,
+    ArgentumGymClient,
+    ArgentumRemoteError,
+)
 from .records import ActionRecord, DecisionRecord, PilotProvenance, RecordValidationError
 
 __all__ = [
     "ActionRecord",
     "ArgentumCanonicalDecision",
     "ArgentumCanonicalError",
+    "ArgentumClientConfigurationError",
+    "ArgentumClientError",
+    "ArgentumConnectionError",
+    "ArgentumDeliveryUnknownError",
+    "ArgentumGymClient",
+    "ArgentumRemoteError",
     "DecisionRecord",
     "PilotProvenance",
     "RecordValidationError",
